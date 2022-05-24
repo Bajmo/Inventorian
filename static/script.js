c62e7551ddf4a84cb27a7a3187097784ef302876
Bajmo <96373893+Bajmo@users.noreply.github.com>
2022-05-24 18:51:04 +0100

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sideBar").style.width = "250px";
-    document.getElementById("toggle_dropdown").classList.remove("dropdown-toggle");
-}
-
-function closeNav() {
-    document.getElementById("sideBar").style.width = "0";
-    document.getElementById("toggle_dropdown").classList.add("dropdown-toggle");
-}
